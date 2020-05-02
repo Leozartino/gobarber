2627c93ee25a8b547d8891a111742612b4ca3c44
Leozartino <leozartino@gmail.com>
2020-05-02 20:15:02 -0300

    feat(Appointments.ts): ajustando o model

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,4 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 // No caso de classes, por já esta declarado os atributos
 // não é necessário criar uma interface para extrair de um objeto especifico
 // mas sim, só usar a sintaxe de desestruturação e falar quais atributos
@@ -14,6 +20,12 @@ class Appointment {
 
   @Column('time with time zone')
   date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 
 export default Appointment;
