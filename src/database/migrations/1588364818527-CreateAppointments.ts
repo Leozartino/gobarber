eaac789a6c935bff98270298357285d6fb93d534
Leozartino <leozartino@gmail.com>
2020-05-02 17:10:41 -0300

    feat(migration): alterando default de id uuid

--- src/database/migrations/1588364818527-CreateAppointments.ts
+++ src/database/migrations/1588364818527-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1588361842058
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
