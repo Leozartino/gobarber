58007b3108803865cf8846ba9919faf4d2c6f768
Leozartino <leozartino@gmail.com>
2020-05-02 21:43:01 -0300

    feat(routes): add logica inicial de users routes

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+/* import UsersRepository from '../repositories/UsersRepository';
+import CreateUserService from '../services/CreateUserService';
+ */
+
+const usersRouter = Router();
+
+export default usersRouter;
