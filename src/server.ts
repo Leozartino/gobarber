88fd599b8ee64b95c8158b1104c4b00f8ade3b51
Leozartino <leozartino@gmail.com>
2020-05-04 17:46:43 -0300

    feat(server.ts): add middleware de erros globais
    Foi adicionado a dependencia: express-async-errros, além da implementação do middleware com as
    tratativas de erros utilizado da classe AppError

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
-import './database';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
+
+import './database';
 
 const app = express();
 const port = 3333;
@@ -10,6 +13,25 @@ const port = 3333;
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    // verifica se o erro é uma instância da classe AppError
+    // se for quer dizer que foi originado pela aplicação, erro conhecido
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
+
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Server is running on port: ${port} 🎢`);
