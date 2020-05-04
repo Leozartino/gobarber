76ea51f2cb23eef3958573724889be7d718cf35b
Leonardo Oliveira <leozartino@gmail.com>
2020-05-04 11:00:36 -0300

    feat(user.routes.ts): implementado a rota de upload de avatar PATCH

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -18,8 +21,13 @@ usersRouter.post('/', async (request, response) => {
   }
 });
 
-usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
-  response.json({ ok: true });
-});
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
