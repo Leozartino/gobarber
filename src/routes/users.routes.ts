dd9a3d248987b8854908c76319fa41cb0c52982d
Leonardo Oliveira <leozartino@gmail.com>
2020-05-04 10:35:56 -0300

    feat(users.routes.ts): add rota patach para upload de avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+
 usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body;
@@ -14,6 +16,10 @@ usersRouter.post('/', async (request, response) => {
   } catch (error) {
     return response.status(400).json({ error: error.message });
   }
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
+  response.json({ ok: true });
 });
 
 export default usersRouter;
