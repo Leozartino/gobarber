315a8a9989d69190507a5ebd046b2aaf642a2e65
Leozartino <leozartino@gmail.com>
2020-06-15 19:56:44 -0300

    refactor: change tag html form to Form (unform)

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Form } from '@unform/web';
+
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Container, Content, Background } from './styles';
 import logo from '../../assets/logo.svg';
+
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
   return (
     <>
       <Container>
         <Background />
         <Content>
           <img src={logo} alt="go-barber" />
-          <form>
+          <Form onSubmit={handleSubmit}>
             <h1>Faça seu cadastro</h1>
             <Input name="name" icon={FiUser} placeholder="Nome" />
             <Input name="email" icon={FiMail} placeholder="E-mail" />
@@ -23,7 +30,7 @@ const SignUp: React.FC = () => {
               placeholder="Senha"
             />
             <Button type="submit">Cadastrar</Button>
-          </form>
+          </Form>
           <a href="forgot">
             <FiArrowLeft />
             Voltar para logon
